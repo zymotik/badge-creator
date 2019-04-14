c2b6bf5c9ba3b55714aa8b9cd74f98461e01e04f
Ian Brown <email@ian-b.com>
2019-04-14 10:57:18 +0100

    add generated date to excel file once processed

--- excel.js
+++ excel.js
@@ -1,8 +1,11 @@
 const xlsx = require('xlsx');
 
 module.exports = {
-    getRows: getRows
+    getRows: getRows,
+    saveRows: saveRows,
 }
+
+const xlsxOptions = { cellStyles: true };
 
 /**
  * 
@@ -10,9 +13,15 @@ module.exports = {
  * @returns { [ { AccessRequired: string, Department: string, Person: string, Timestamp: number, Generated: Date }] }
  */
 function getRows(filename) {
-    const workbook = xlsx.readFile(filename);
+    const workbook = xlsx.readFile(filename, xlsxOptions);
     const worksheet = workbook.Sheets[workbook.SheetNames[0]];
     const cells = xlsx.utils.sheet_to_json(worksheet);
     return cells;
 }
 
+function saveRows(rows, filename) {
+    const workbook = xlsx.readFile(filename, xlsxOptions);
+    const worksheet = xlsx.utils.json_to_sheet(rows);
+    workbook.Sheets[workbook.SheetNames[0]] = worksheet;
+    xlsx.writeFile(workbook, filename, xlsxOptions);
+}
--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const excel = require('./excel');
 const colors = require('colors');
 const moment = require('moment-mini');
 
+const excelPassListFile = './pass-list.xlsx';
+
 function main() {
-    const rows = excel.getRows('pass-list.xlsx');
+    // TODO: create required folders if they do not exist
+    const timestamp = moment();
+    const rows = excel.getRows(excelPassListFile);
     const accessEarly = rows.filter((r) => !r.Generated && simplifyString(r.AccessRequired) === 'early');
     const accessBackstage = rows.filter((r) => !r.Generated && simplifyString(r.AccessRequired) === 'backstage');
     const accessEarlyBackstage = rows.filter((r) => !r.Generated && simplifyString(r.AccessRequired) === 'earlybackstage');
@@ -15,17 +19,22 @@ function main() {
                 ${colors.green(accessBackstage.length.toString().padStart(3, '0'))} backstage passes
                 ${colors.green(accessEarlyBackstage.length.toString().padStart(3, '0'))} early & backstage passes`);
 
-    accessEarly.length > 0 && generateFile('template-early', accessEarly);
-    accessBackstage.length > 0 && generateFile('template-backstage', accessBackstage);
-    accessEarlyBackstage.length > 0 && generateFile('template-early-backstage', accessEarlyBackstage);
+    accessEarly.length > 0 && generateFile('template-early', accessEarly, timestamp);
+    accessBackstage.length > 0 && generateFile('template-backstage', accessBackstage, timestamp);
+    accessEarlyBackstage.length > 0 && generateFile('template-early-backstage', accessEarlyBackstage, timestamp);
 
-    // save those generated
+    markRowsAsGenerated([...accessEarly, ...accessBackstage, ...accessEarlyBackstage], timestamp);
+    
+    excel.saveRows(rows, excelPassListFile);
 }
 
-function generateFile(templateFile, passList) {
+function markRowsAsGenerated(passList, timestamp) {
+    passList.map(pass => pass.Generated = timestamp.toDate());
+}
+
+function generateFile(templateFile, passList, timestamp) {
     const totalCount = passList.length;
     const passesPerPage = 10;
-    const timestamp = moment();
     let page = 1;
     const generatedFiles = [];
 
